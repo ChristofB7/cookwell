0ce0fcbea67689840e09190aac65b846acfb6336
Christof Belisle <Christof@jazab.com>
2020-11-24 12:01:50 -0500

    Revert "commented out warnings"
    This reverts commit be69d3ef85eeeb9fa9c18cd641f6d96d168044de, reversing
    changes made to 09b401aa27e054c9d1db576f2f718069460f75c8.

--- lib/model/shopping_item.dart
+++ lib/model/shopping_item.dart
@@ -1,47 +1,13 @@
-import 'package:cookwell/db/shopping_db_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 class ShoppingItem {
-  int id;
-  String item;
+  String name;
   int quantity;
-  bool checked;
+  String checked;
 
-  ShoppingItem({this.id,
-    @required this.item,
+  ShoppingItem({@required this.name,
     @required this.quantity,
     @required this.checked,
   });
-
-  Map<String, dynamic> toMap() =>{
-    ShoppingDatabaseProvider.COLUMN_ID: id,
-    ShoppingDatabaseProvider.COLUMN_ITEM: item,
-    ShoppingDatabaseProvider.COLUMN_CHECKED: checked ? 1 : 0,
-    ShoppingDatabaseProvider.COLUMN_QUANTITY: quantity,
-    };
-
-  ShoppingItem.fromMap(Map<String, dynamic> map)   {
-    id = map[ShoppingDatabaseProvider.COLUMN_ID];
-    item = map[ShoppingDatabaseProvider.COLUMN_ITEM];
-    quantity = map[ShoppingDatabaseProvider.COLUMN_QUANTITY];
-    checked = map[ShoppingDatabaseProvider.COLUMN_CHECKED] == 1;
-  }
-
-  static Future<String> createShoppingList() async {
-    List<ShoppingItem> list = await ShoppingDatabaseProvider.getUncheckedItems();
-
-    String sharableList = "";
-
-    for (ShoppingItem item in list) {
-      sharableList += item.toString();
-    }
-
-    return sharableList;
-  }
-
-  String toString(){
-    return ("${this.item} ${this.quantity == 0 ? "" : "(${this.quantity})"}\n");
-  }
-
 }
